c164e165257cab0e443df8c074c9231c02001299
richardjgowers <richardjgowers@gmail.com>
2019-02-11 14:09:18 -0600

    trying to use blas

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,6 @@
 cd $SRC_DIR/tightbind
-cmake -DCMAKE_C_FLAGS="-O3" .
+cmake -DUSE_BLAS_LAPACK=ON -DCMAKE_C_FLAGS="-O3" -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX .
 make
+make install
 # Makefile can't do custom --prefix, do manual make install
-mv bind eht_bind
-mkdir -p $PREFIX/bin
-cp eht_bind $PREFIX/bin
+mv $PREFIX/bin/bind $PREFIX/bin/eht_bind
