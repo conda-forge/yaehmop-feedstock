c79c0cc50cddbc045f518a57dd5fbef272274e88
richardjgowers <richardjgowers@gmail.com>
2019-02-13 11:35:08 -0600

    removing blas for now again..

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 cd $SRC_DIR/tightbind
-cmake -DUSE_BLAS_LAPACK=ON -DCMAKE_C_FLAGS="-O3" .
+cmake -DCMAKE_C_FLAGS="-O3" .
+# cmake -DUSE_BLAS_LAPACK=ON -DCMAKE_C_FLAGS="-O3" .
 make
 # Makefile can't do custom --prefix, do manual make install
 mv bind eht_bind
